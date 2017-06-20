a70e63dd9a5880fd613b2c7fedf4c5ca556dd6ea
Victor <victor@sene.ovh>
2017-06-20 23:18:29 +0200

    useless changes on DEFINE

--- inc/main.h
+++ inc/main.h
@@ -18,13 +18,14 @@
 #define TEMP_OUT_L 0x2A
 #define TEMP_OUT_H 0x2B
 
-#define T0_OUT_L 0x3C
-#define T0_OUT_R 0x3D
-#define T1_OUT_L 0x3E
-#define T1_OUT_R 0x3F
-
 #define T0_degC_x8 0x32
 #define T1_degC_x8 0x33
+#define T_msb 0x35
+
+#define T0_OUT_L 0x3C
+#define T0_OUT_H 0x3D
+#define T1_OUT_L 0x3E
+#define T1_OUT_H 0x3F
 
 /* Exported functions ------------------------------------------------------- */
 void set_clk(void);
